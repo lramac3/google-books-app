ee4fb0c42e2fea03a3f41e8cb2afc90699365793
Linu Ramachandran <linuram@gmail.com>
2019-04-13 23:48:23 -0400

    first commit

---
+++ create-react-express/client/build/precache-manifest.8a55202286b923dfb2465f226ecdd2db.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "495d93cc2c05774c4e88882137125113",
+    "url": "/static/media/logo.495d93cc.svg"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "5f5fc2fae6b366ae3296",
+    "url": "/static/js/main.24ea1a14.chunk.js"
+  },
+  {
+    "revision": "45124f853ba07aa965c0",
+    "url": "/static/js/2.6efc73d3.chunk.js"
+  },
+  {
+    "revision": "5f5fc2fae6b366ae3296",
+    "url": "/static/css/main.7e89db99.chunk.css"
+  },
+  {
+    "revision": "3df263b304910ad1951c3a0b4d2ce4fd",
+    "url": "/index.html"
+  }
+];
---
+++ create-react-express/server.js
@@ -0,0 +1,43 @@
+const express = require("express");
+const mongoose = require('mongoose');
+const bodyParser=require('body-parser');
+const path = require("path");
+
+const app = express();
+const log = console.log;;
+
+const routes = require('./routes');
+
+const app=express()
+const PORT = process.env.PORT || 3001
+
+mongoose.connect(
+  process.env.MONGODB_URI || 'mongodb://localhost/my_database',
+  { useNewUrlParser: true }
+)
+
+// Define middleware here
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }))
+app.use('/', routes)
+
+// Serve up static assets (usually on heroku)
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static("client/build"));
+  app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname,'client','build','index.html'));
+  }
+}
+
+// // Define API routes here
+
+// // Send every other request to the React app
+// // Define any API routes before this runs
+// app.get("*", (req, res) => {
+//   res.sendFile(path.join(__dirname, "./client/build/index.html"));
+// });
+
+app.listen(PORT, () => {
+  log(`🌎 ==> API server now on port ${PORT}!`);
+});
