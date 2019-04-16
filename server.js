1d8619bc2792e0e2c83a1e437aa2c50520ec1704
Linu Ramachandran <linuram@gmail.com>
2019-04-16 01:41:01 -0400

    updated navbar

--- server.js
+++ server.js
@@ -1,5 +1,5 @@
 const express = require("express");
-const mongoose = require('mongoose');
+// const mongoose = require('mongoose');
 const bodyParser=require('body-parser');
 const path = require("path");
 
@@ -11,24 +11,24 @@ const routes = require('./routes');
 const app=express()
 const PORT = process.env.PORT || 3001
 
-mongoose.connect(
-  process.env.MONGODB_URI || 'mongodb://localhost/my_database',
-  { useNewUrlParser: true }
-)
+// mongoose.connect(
+//   process.env.MONGODB_URI || 'mongodb://localhost/my_database',
+//   { useNewUrlParser: true }
+// )
 
 // Define middleware here
 
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: false }))
+// app.use(bodyParser.json());
+// app.use(bodyParser.urlencoded({ extended: false }))
 app.use('/', routes)
 
 // Serve up static assets (usually on heroku)
 if (process.env.NODE_ENV === "production") {
   app.use(express.static("client/build"));
-  app.get('*', (req, res) => {
-    res.sendFile(path.join(__dirname,'client','build','index.html'));
-  }
-}
+  // app.get('*', (req, res) => {
+  //   res.sendFile(path.join(__dirname,'client','build','index.html'));
+  // }
+};
 
 // // Define API routes here
 
